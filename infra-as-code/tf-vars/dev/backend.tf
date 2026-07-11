8a7fcfb2016c68d302a1fe03c7aebd4c34ba5cde
Anshul Garg <anshulgarg.cse@gmail.com>
2026-07-11 08:30:48 +0530

    Initial Setup (#1)

---
+++ infra-as-code/tf-vars/dev/backend.tf
@@ -0,0 +1,4 @@
+bucket       = "aws-commons-dev-terraform-state"
+key          = "agent-runtime-labs/talent-mcp-server/dev/756375699536/us-east-1/terraform.tfstate"
+use_lockfile = true
+region       = "us-east-1"
